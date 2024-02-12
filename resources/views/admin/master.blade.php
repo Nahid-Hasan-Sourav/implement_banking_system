97bdff63d8ae568770d7e8c2a39e42b654eca90f
Nahid-Hasan-Sourav <sourav@excelitai.com>
2024-02-12 11:51:57 +0600

    successfully complete user register and login

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -37,7 +37,13 @@
             </a>
             <div class="dropdown-menu dropdown-menu-header wd-200">
               <ul class="list-unstyled user-profile-nav">
-                <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                <li onclick="document.getElementById('logOutBtn').submit()">
+                  <a href="#"><i class="icon ion-power"></i> Sign Out</a>
+              </li>
+
+              <form action="{{ route('logout') }}" method="POST" id="logOutBtn">
+                  @csrf
+              </form>
               </ul>
             </div><!-- dropdown-menu -->
           </div><!-- dropdown -->
@@ -61,18 +67,7 @@
         @yield('body')
        </div>
 
-      </div><!-- sl-pagebody -->
-      {{-- <footer class="sl-footer">
-        <div class="footer-left">
-          <div class="mg-b-2">Copyright &copy; 2017. Starlight. All Rights Reserved.</div>
-          <div>Made by ThemePixels.</div>
-        </div>
-        <div class="footer-right d-flex align-items-center">
-          <span class="tx-uppercase mg-r-10">Share:</span>
-          <a target="_blank" class="pd-x-5" href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/starlight"><i class="fa fa-facebook tx-20"></i></a>
-          <a target="_blank" class="pd-x-5" href="https://twitter.com/home?status=Starlight,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/starlight"><i class="fa fa-twitter tx-20"></i></a>
-        </div>
-      </footer> --}}
+      </div>
     </div><!-- sl-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
 
